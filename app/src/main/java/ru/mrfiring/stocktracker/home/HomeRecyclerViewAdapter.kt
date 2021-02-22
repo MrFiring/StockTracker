879608966a3e3508a2eee91aad2b9f190839bb39
Kirill Bondarev <Gut54566@yandex.ru>
2021-02-22 19:55:26 +0700

    HomeRecyclerViewAdapter.kt added ViewHolder for DomainStockSymbol. HomeRecyclerViewAdapter.kt added ClickListener stock_list_item.xml added 2 variables

--- app/src/main/java/ru/mrfiring/stocktracker/home/HomeRecyclerViewAdapter.kt
+++ app/src/main/java/ru/mrfiring/stocktracker/home/HomeRecyclerViewAdapter.kt
@@ -1,11 +1,52 @@
 package ru.mrfiring.stocktracker.home
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import ru.mrfiring.stocktracker.repository.network.StockSymbol
+import ru.mrfiring.stocktracker.databinding.StockListItemBinding
+import ru.mrfiring.stocktracker.repository.domain.DomainStockSymbol
 
-class HomeRecyclerViewAdapter : ListAdapter<StockSymbol, >() {
+class HomeRecyclerViewAdapter(val clickListener: HomeRecyclerViewAdapter.ClickListener) : ListAdapter<DomainStockSymbol, HomeRecyclerViewAdapter.StockViewHolder>(StockSymbolDiffCallback()) {
 
-    class StockViewHolder private constructor(val binding: DataBinding) : RecyclerView.ViewHolder()
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StockViewHolder {
+        return StockViewHolder.from(parent)
+    }
 
+    override fun onBindViewHolder(holder: StockViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item, clickListener)
+    }
+
+    class StockViewHolder private constructor(val binding: StockListItemBinding) : RecyclerView.ViewHolder(binding.root){
+        fun bind(item: DomainStockSymbol, clickListener: ClickListener){
+            binding.stockSymbol = item
+            binding.clickListener = clickListener
+            binding.executePendingBindings()
+        }
+
+        companion object{
+            fun from(parent: ViewGroup): StockViewHolder{
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = StockListItemBinding.inflate(layoutInflater, parent, false)
+
+                return StockViewHolder(binding)
+            }
+        }
+    }
+
+    class StockSymbolDiffCallback: DiffUtil.ItemCallback<DomainStockSymbol>(){
+        override fun areItemsTheSame(oldItem: DomainStockSymbol, newItem: DomainStockSymbol): Boolean {
+            return oldItem.symbol == newItem.symbol
+        }
+
+        override fun areContentsTheSame(oldItem: DomainStockSymbol, newItem: DomainStockSymbol): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    class ClickListener(val block: (DomainStockSymbol) -> Unit){
+        fun onClick(stock: DomainStockSymbol) = block(stock)
+    }
 }
