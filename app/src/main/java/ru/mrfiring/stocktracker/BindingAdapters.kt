077bc3c3511bad08c08dd1102ec8caa695845470
Kirill Bondarev <Gut54566@yandex.ru>
2021-02-24 16:17:12 +0700

    Added BindingAdapter for loading company's logo

--- app/src/main/java/ru/mrfiring/stocktracker/BindingAdapters.kt
+++ app/src/main/java/ru/mrfiring/stocktracker/BindingAdapters.kt
@@ -1,7 +1,11 @@
 package ru.mrfiring.stocktracker
 
+import android.widget.ImageView
+import androidx.core.net.toUri
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import ru.mrfiring.stocktracker.home.HomeRecyclerViewAdapter
 import ru.mrfiring.stocktracker.repository.domain.DomainStockSymbol
 
@@ -9,4 +13,17 @@ import ru.mrfiring.stocktracker.repository.domain.DomainStockSymbol
 fun RecyclerView.setListData(list: List<DomainStockSymbol>?){
     val adpt = adapter as HomeRecyclerViewAdapter
     adpt.submitList(list)
+}
+
+@BindingAdapter("imageUrl")
+fun ImageView.bindImage(imgUrl: String?){
+    imgUrl?.let {
+        val imgUri = imgUrl.toUri().buildUpon().scheme("https").build()
+        Glide.with(context)
+                .load(imgUri)
+                .apply(RequestOptions()
+                        .placeholder(R.drawable.ic_launcher_background)
+                        .error(android.R.drawable.ic_dialog_alert))
+                .into(this)
+    }
 }
