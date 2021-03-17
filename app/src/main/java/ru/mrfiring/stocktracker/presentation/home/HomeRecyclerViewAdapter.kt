c5a70fecd331cd7ec1ea575372bb132b5cdd653e
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-17 23:19:48 +0700

    Merge issue fixed

--- app/src/main/java/ru/mrfiring/stocktracker/presentation/home/HomeRecyclerViewAdapter.kt
+++ app/src/main/java/ru/mrfiring/stocktracker/presentation/home/HomeRecyclerViewAdapter.kt
@@ -1,5 +1,6 @@
 package ru.mrfiring.stocktracker.presentation.home
 
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.core.net.toUri
@@ -10,10 +11,11 @@ import com.avatarfirst.avatargenlib.AvatarConstants
 import com.avatarfirst.avatargenlib.AvatarGenerator
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import ru.mrfiring.stocktracker.R
 import ru.mrfiring.stocktracker.databinding.StockListItemBinding
 import ru.mrfiring.stocktracker.domain.DomainStockSymbol
 
-class HomeRecyclerViewAdapter(val clickListener: ClickListener) :
+class HomeRecyclerViewAdapter(private val clickListener: ClickListener) :
     ListAdapter<DomainStockSymbol, HomeRecyclerViewAdapter.StockViewHolder>(
         StockSymbolDiffCallback()
     ) {
@@ -32,15 +34,34 @@ class HomeRecyclerViewAdapter(val clickListener: ClickListener) :
     ) : RecyclerView.ViewHolder(binding.root) {
         fun bind(item: DomainStockSymbol, clickListener: ClickListener) {
             binding.apply {
+                val context = root.context
+
                 root.setOnClickListener {
                     clickListener.onClick(item)
                 }
+
                 companyName.text = item.companyName
                 symbol.text = item.symbol
-                currentPrice.text = item.currentPrice.toString()
-                deltaPrice.text = item.deltaPrice.toString()
 
-                val context = root.context
+                val curPrice = item.quote.current
+                currentPrice.text = curPrice.toString()
+
+                if (curPrice > 0) {
+                    val dPrice = item.quote.getDeltaPrice()
+
+                    if (dPrice > 0) {
+                        deltaPrice.setTextColor(Color.GREEN)
+                    } else {
+                        deltaPrice.setTextColor(Color.RED)
+                    }
+
+                    deltaPrice.text = context.getString(
+                        R.string.delta_price_format,
+                        dPrice,
+                        item.quote.getDeltaPricePercent()
+                    )
+                }
+
                 val imgUri = item.logoUrl.toUri().buildUpon().scheme("https").build()
                 Glide.with(context)
                     .load(imgUri)
