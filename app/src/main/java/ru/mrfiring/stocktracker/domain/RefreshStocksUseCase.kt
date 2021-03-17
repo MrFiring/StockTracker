ffcc0e676deea45892c5af9903cc1c47c371b854
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-18 00:27:46 +0700

    RefreshStocksUseCase.kt removed

--- app/src/main/java/ru/mrfiring/stocktracker/domain/RefreshStocksUseCase.kt
+++
@@ -1,9 +0,0 @@
-package ru.mrfiring.stocktracker.domain
-
-import javax.inject.Inject
-
-class RefreshStocksUseCase @Inject constructor(
-    private val stockRepository: StockRepository
-) {
-    suspend operator fun invoke() = stockRepository.refreshStocks()
-}
