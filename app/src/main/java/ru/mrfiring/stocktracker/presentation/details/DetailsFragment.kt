c0e87a77b9eb7a4dcae058338ec5fd4cd363b194
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-18 14:16:51 +0700

    Details implemented with args

--- app/src/main/java/ru/mrfiring/stocktracker/presentation/details/DetailsFragment.kt
+++ app/src/main/java/ru/mrfiring/stocktracker/presentation/details/DetailsFragment.kt
@@ -5,21 +5,100 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.get
 import dagger.hilt.android.AndroidEntryPoint
+import ru.mrfiring.stocktracker.R
 import ru.mrfiring.stocktracker.databinding.DetailsFragmentBinding
+import ru.mrfiring.stocktracker.domain.DomainCompany
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class DetailsFragment : Fragment() {
+
+    @Inject
+    lateinit var detailsViewModelFactory: DetailsViewModel.AssistedFactory
+
+    private lateinit var binding: DetailsFragmentBinding
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: DetailsFragmentBinding =
-            DetailsFragmentBinding.inflate(inflater, container, false)
+        binding = DetailsFragmentBinding.inflate(inflater, container, false)
+        val argSymbol = DetailsFragmentArgs.fromBundle(requireArguments()).symbol
+        val viewModel: DetailsViewModel = ViewModelProvider(
+            this,
+            DetailsViewModel.provideFactory(
+                detailsViewModelFactory,
+                argSymbol
+            )
+        ).get()
 
+        viewModel.company.observe(viewLifecycleOwner) {
+            setupInformationCard(it)
+        }
+
+        viewModel.status.observe(viewLifecycleOwner) {
+            when (it) {
+                LoadingStatus.LOADING -> {
+                    setIsLoading()
+                }
+                LoadingStatus.DONE -> {
+                    setIsLoaded()
+                }
+                else -> {
+
+                }
+            }
+        }
 
         return binding.root
+    }
+
+    private fun setupInformationCard(company: DomainCompany) {
+        binding.apply {
+            detailCompanyName.text = company.name
+            detailTicker.text = company.symbol
+
+            detailPhone.text = getString(
+                R.string.format_phone,
+                company.phone
+            )
+            detailExchange.text = getString(
+                R.string.format_exchange,
+                company.exchange
+            )
+            detailIndustry.text = getString(
+                R.string.format_industry,
+                company.finhubIndustry
+            )
+
+
+            detailMarketCap.text = getString(
+                R.string.format_market_cap,
+                company.marketCapitalization
+            )
+            detailOutstanding.text = getString(
+                R.string.format_number_outstanding,
+                company.shareOutStanding
+            )
+
+
+        }
+
+
+    }
+
+    private fun setIsLoading() {
+        binding.detailLoadingBar.visibility = View.VISIBLE
+        binding.companyCard.visibility = View.GONE
+    }
+
+    private fun setIsLoaded() {
+        binding.detailLoadingBar.visibility = View.GONE
+        binding.companyCard.visibility = View.VISIBLE
     }
 
 }
--- app/src/main/java/ru/mrfiring/stocktracker/presentation/details/DetailsViewModel.kt
+++ app/src/main/java/ru/mrfiring/stocktracker/presentation/details/DetailsViewModel.kt
@@ -1,21 +1,25 @@
 package ru.mrfiring.stocktracker.presentation.details
 
 import android.app.Application
-import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.*
+import dagger.assisted.Assisted
+import dagger.assisted.AssistedInject
 import kotlinx.coroutines.launch
 import ru.mrfiring.stocktracker.domain.DomainCompany
+import ru.mrfiring.stocktracker.domain.FetchCompanyUseCase
+import ru.mrfiring.stocktracker.domain.GetCompanyBySymbolUseCase
 import java.io.IOException
-import javax.inject.Inject
 
 enum class LoadingStatus {
     LOADING, ERROR, DONE
 }
 
-class DetailsViewModel @Inject constructor(
-    application: Application
+
+class DetailsViewModel @AssistedInject constructor(
+    application: Application,
+    @Assisted private var symbol: String,
+    private val getCompanyBySymbolUseCase: GetCompanyBySymbolUseCase,
+    private val fetchCompanyUseCase: FetchCompanyUseCase
 ) : AndroidViewModel(application) {
 
     private val _status = MutableLiveData<LoadingStatus>()
@@ -27,13 +31,40 @@ class DetailsViewModel @Inject constructor(
         get() = _company
 
 
-    fun bindCompany() = viewModelScope.launch {
+    init {
+        bindCompany()
+    }
+
+    private fun bindCompany() = viewModelScope.launch {
         try {
             _status.value = LoadingStatus.LOADING
-            //to task
-            _status.value = LoadingStatus.DONE
+            fetchCompanyUseCase(symbol)
+            val companyInfo = getCompanyBySymbolUseCase(symbol)
+            companyInfo?.let {
+                _company.value = it
+                _status.value = LoadingStatus.DONE
+            }
         } catch (ex: IOException) {
             _status.value = LoadingStatus.ERROR
+        }
+    }
+
+
+    @dagger.assisted.AssistedFactory
+    interface AssistedFactory {
+        fun create(symbol: String): DetailsViewModel
+    }
+
+    companion object {
+        fun provideFactory(
+            assistedFactory: AssistedFactory,
+            symbol: String
+        ): ViewModelProvider.Factory = object : ViewModelProvider.Factory {
+            @Suppress("UNCHECKED_CAST")
+            override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+                return assistedFactory.create(symbol) as T
+            }
+
         }
     }
 
