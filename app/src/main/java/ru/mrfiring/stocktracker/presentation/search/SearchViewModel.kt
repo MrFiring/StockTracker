d71e072d227c26c027fbf31c1a5b8ea39fb42aca
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-18 10:59:08 +0700

    ViewModelScoped annotation removed

--- app/src/main/java/ru/mrfiring/stocktracker/presentation/search/SearchViewModel.kt
+++ app/src/main/java/ru/mrfiring/stocktracker/presentation/search/SearchViewModel.kt
@@ -1,9 +1,7 @@
 package ru.mrfiring.stocktracker.presentation.search
 
 import androidx.lifecycle.ViewModel
-import dagger.hilt.android.scopes.ViewModelScoped
 
-@ViewModelScoped
 class SearchViewModel : ViewModel() {
     // TODO: Implement the ViewModel
 }
