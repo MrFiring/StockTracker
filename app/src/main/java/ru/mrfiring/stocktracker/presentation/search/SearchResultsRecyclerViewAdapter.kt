6c85c4da4cbe602297512f2aead1536ebded6da6
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-20 15:34:02 +0700

    SearchResultsRecyclerViewAdapter.kt implemented

---
+++ app/src/main/java/ru/mrfiring/stocktracker/presentation/search/SearchResultsRecyclerViewAdapter.kt
@@ -0,0 +1,21 @@
+package ru.mrfiring.stocktracker.presentation.search
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import ru.mrfiring.stocktracker.domain.DomainStockSymbol
+import ru.mrfiring.stocktracker.presentation.reusable.diffutil.StockSymbolDiffUtilCallback
+import ru.mrfiring.stocktracker.presentation.reusable.viewholder.StockViewHolder
+
+class SearchResultsRecyclerViewAdapter(
+    private val onClick: (DomainStockSymbol) -> Unit
+) : ListAdapter<DomainStockSymbol, StockViewHolder>(StockSymbolDiffUtilCallback()) {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): StockViewHolder {
+        return StockViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: StockViewHolder, position: Int) {
+        val item = getItem(position)
+        holder.bind(item, onClick)
+    }
+
+}
