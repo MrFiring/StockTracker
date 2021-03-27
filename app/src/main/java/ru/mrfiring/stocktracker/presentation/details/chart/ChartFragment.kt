6acefe71bec232a94e9c7b8e29dffd7b350ef658
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-27 18:59:07 +0700

    Chart beta implementation)

--- app/src/main/java/ru/mrfiring/stocktracker/presentation/details/chart/ChartFragment.kt
+++ app/src/main/java/ru/mrfiring/stocktracker/presentation/details/chart/ChartFragment.kt
@@ -1,13 +1,22 @@
 package ru.mrfiring.stocktracker.presentation.details.chart
 
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.github.mikephil.charting.components.YAxis
+import com.github.mikephil.charting.data.CandleData
+import com.github.mikephil.charting.data.CandleDataSet
+import com.github.mikephil.charting.data.CandleEntry
 import dagger.hilt.android.AndroidEntryPoint
+import ru.mrfiring.stocktracker.R
 import ru.mrfiring.stocktracker.databinding.FragmentChartBinding
+import ru.mrfiring.stocktracker.domain.DomainStockCandles
+import ru.mrfiring.stocktracker.presentation.details.general.LoadingStatus
 
 private const val ARG_PARAM = "symbol"
 
@@ -24,9 +33,114 @@ class ChartFragment : Fragment() {
     ): View? {
         binding = FragmentChartBinding.inflate(inflater, container, false)
 
+
+        //Setup chart base parameters
+        with(binding.candleChart) {
+            requestDisallowInterceptTouchEvent(true)
+            legend.isEnabled = false
+            xAxis.setDrawGridLines(false)
+            xAxis.setDrawLabels(false)
+            xAxis.setAvoidFirstLastClipping(true)
+        }
+
+        //Listen to button's toggle
+        binding.chartResolutionGroup.addOnButtonCheckedListener { group, checkedId, isChecked ->
+            when (checkedId) {
+                R.id.chartResolutionDay -> {
+                    viewModel.bindData(CandlesResolution.DAY)
+                }
+                R.id.chartResolutionSixtyMin -> {
+                    viewModel.bindData(CandlesResolution.SIXTY_MIN)
+                }
+                R.id.chartResolutionFiveMin -> {
+                    viewModel.bindData(CandlesResolution.FIVE_MIN)
+                }
+                R.id.chartResolutionOneMin -> {
+                    viewModel.bindData(CandlesResolution.ONE_MIN)
+                }
+            }
+        }
+
+        //Toggle button to load chart on startup.
+        binding.chartResolutionDay.toggle()
+
+        //Setup chart when data arrives
+        viewModel.candles.observe(viewLifecycleOwner) {
+            setupChart(it)
+        }
+
+        //Loading status observe
+        viewModel.status.observe(viewLifecycleOwner) {
+            when (it) {
+                LoadingStatus.LOADING -> {
+                    setIsLoading()
+                }
+                LoadingStatus.DONE -> {
+                    setIsLoaded()
+                }
+                LoadingStatus.ERROR -> {
+                    setIsError()
+                }
+            }
+        }
+
         return binding.root
     }
 
+    private fun setupChart(candles: DomainStockCandles) {
+        val entries = mutableListOf<CandleEntry>()
+        entries.addAll(
+            candles.timestamp.mapIndexed { i, _ ->
+                CandleEntry(
+                    i.toFloat(),
+                    candles.high[i].toFloat(), candles.low[i].toFloat(),
+                    candles.open[i].toFloat(), candles.close[i].toFloat()
+                )
+            }
+        )
+
+        val candleDataSet = CandleDataSet(entries, "Candles")
+        with(candleDataSet) {
+            axisDependency = YAxis.AxisDependency.LEFT
+            shadowColor = Color.DKGRAY
+            shadowWidth = 1f
+            decreasingColor = Color.RED
+            decreasingPaintStyle = Paint.Style.FILL
+            increasingColor = Color.GREEN
+            increasingPaintStyle = Paint.Style.FILL
+            neutralColor = Color.BLACK
+
+        }
+
+        val candleData = CandleData(candleDataSet)
+
+        binding.candleChart.data = candleData
+        binding.candleChart.setVisibleXRangeMaximum(20f)
+        binding.candleChart.invalidate()
+    }
+
+    private fun setIsLoading() {
+        with(binding) {
+            chartProgressBar.visibility = View.VISIBLE
+            chartNetworkError.networkErrorContainer.visibility = View.GONE
+            candleChart.visibility = View.GONE
+        }
+    }
+
+    private fun setIsLoaded() {
+        with(binding) {
+            candleChart.visibility = View.VISIBLE
+            chartProgressBar.visibility = View.GONE
+        }
+    }
+
+    private fun setIsError() {
+        with(binding) {
+            chartNetworkError.networkErrorContainer.visibility = View.VISIBLE
+            chartProgressBar.visibility = View.GONE
+            candleChart.visibility = View.GONE
+        }
+    }
 
     companion object {
         @JvmStatic
--- app/src/main/java/ru/mrfiring/stocktracker/presentation/details/chart/ChartViewModel.kt
+++ app/src/main/java/ru/mrfiring/stocktracker/presentation/details/chart/ChartViewModel.kt
@@ -1,21 +1,23 @@
 package ru.mrfiring.stocktracker.presentation.details.chart
 
 import android.app.Application
+import android.util.Log
 import androidx.lifecycle.*
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import org.joda.time.DateTime
 import ru.mrfiring.stocktracker.domain.DomainStockCandles
 import ru.mrfiring.stocktracker.domain.FetchStockCandlesUseCase
-import ru.mrfiring.stocktracker.domain.GetStockCandlesFlowUseCase
+import ru.mrfiring.stocktracker.domain.GetStockCandlesUseCase
 import ru.mrfiring.stocktracker.presentation.details.general.LoadingStatus
 import java.io.IOException
 import javax.inject.Inject
 
 enum class CandlesResolution(val value: String) {
-    ONE_MIN("1"), FIVE_MIN("5"), FIFTEEN_MIN("15"),
-    THIRTY_MIN("30"), SIXTY_MIN("60"), ONE_DAY("D"),
-    ONE_WEEK("W"), ONE_MONTH("M")
+    ONE_MIN("1"),
+    FIVE_MIN("5"),
+    SIXTY_MIN("30"),
+    DAY("D"),
 }
 
 @HiltViewModel
@@ -23,7 +25,7 @@ class ChartViewModel @Inject constructor(
     application: Application,
     private val savedStateHandle: SavedStateHandle,
     private val fetchStockCandlesUseCase: FetchStockCandlesUseCase,
-    private val getStockCandlesFlowUseCase: GetStockCandlesFlowUseCase
+    private val getStockCandlesUseCase: GetStockCandlesUseCase
 ) : AndroidViewModel(application) {
 
     private val symbol = savedStateHandle.get<String>("symbol") ?: ""
@@ -31,8 +33,6 @@ class ChartViewModel @Inject constructor(
     private val _status = MutableLiveData<LoadingStatus>()
     val status: LiveData<LoadingStatus>
         get() = _status
-
-    private val _resolution = MutableLiveData<CandlesResolution>()
 
     private var _candles = MutableLiveData<DomainStockCandles>()
     val candles: LiveData<DomainStockCandles>
@@ -42,58 +42,23 @@ class ChartViewModel @Inject constructor(
         try {
             _status.value = LoadingStatus.LOADING
 
-            val curUTCTime: Long = DateTime.now().millis / 1000L
+            val curDt = DateTime.now()
+            val curUTCTime: Long = curDt.millis / 1000L
+            val resTime: Long = curDt.minusYears(1).millis / 1000L
             fetchStockCandlesUseCase(
                 symbol = symbol,
                 resolution = resolution.value,
-                fromTime = calculatePeriod(resolution),
+                fromTime = resTime,
                 toTime = curUTCTime
             )
 
-            _candles = getStockCandlesFlowUseCase(
-                symbol,
-                resolution.value
-            ).asLiveData() as MutableLiveData<DomainStockCandles>
+            _candles.value = getStockCandlesUseCase(symbol, resolution.value)
 
             _status.value = LoadingStatus.DONE
         } catch (ex: IOException) {
             _status.value = LoadingStatus.ERROR
+            Log.e("Chart", ex.stackTraceToString())
         }
     }
-
-    fun changeResolution(resolution: CandlesResolution) {
-        _resolution.value = resolution
-    }
-
-    private fun calculatePeriod(resolution: CandlesResolution): Long {
-        val curTime = DateTime.now()
-        return when (resolution) {
-            CandlesResolution.ONE_MIN -> {
-                curTime.minusMinutes(1)
-            }
-            CandlesResolution.FIVE_MIN -> {
-                curTime.minusMinutes(5)
-            }
-            CandlesResolution.FIFTEEN_MIN -> {
-                curTime.minusMinutes(15)
-            }
-            CandlesResolution.THIRTY_MIN -> {
-                curTime.minusMinutes(30)
-            }
-            CandlesResolution.SIXTY_MIN -> {
-                curTime.minusHours(1)
-            }
-            CandlesResolution.ONE_DAY -> {
-                curTime.minusDays(1)
-            }
-            CandlesResolution.ONE_WEEK -> {
-                curTime.minusWeeks(1)
-            }
-            CandlesResolution.ONE_MONTH -> {
-                curTime.minusMonths(1)
-            }
-        }.millis / 1000L
-    }
-
 
 }
